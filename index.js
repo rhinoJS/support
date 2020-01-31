09634dfa98d72177ec963d15263bf103649332f1
BrunoGoncalves <bugotech@gmail.com>
2020-01-31 11:15:07 -0300

    add Num and v0.1.13

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
  */
 module.exports = {
     Arr: require('./src/arr'),
+    Num: require('./src/num'),
     Str: require('./src/str')
 };
---
+++ src/num.js
@@ -0,0 +1,32 @@
+class Num
+{
+    /**
+     * Gerar numero randomico.
+     *
+     * @param min
+     * @param max
+     * @returns Number
+     */
+    static random(min, max) {
+        return Math.random() + (max - min) + min;
+    }
+
+    /**
+     * Gerar numero randomico inteiro.
+     *
+     * @param min
+     * @param max
+     * @returns Number
+     */
+    static randomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+
+        return Math.floor(Math.random() + (max - min) + min);
+    }
+}
+
+/**
+ * Exports.
+ */
+module.exports = Num;
